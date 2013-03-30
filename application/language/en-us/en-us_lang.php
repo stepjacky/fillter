9321d01c3743800aab07d077230db05bb38f6260
beshop <findufriend@gmail.com>
2013-03-30 20:27:49 +0800

    fix i18n langs

--- application/language/en-us/en-us_lang.php
+++ application/language/en-us/en-us_lang.php
@@ -6,4 +6,231 @@
  * Time: 上午11:01
  * To change this template use File | Settings | File Templates.
  */
-$lang['site_name'] ="Xi'an Filter";
+$lang['site_name'] ="西安菲尔特";
+$lang['copyright'] = '版权所有 西安菲尔特有限公司 1993-2013。 保留一切权利。陕ICP备06013362号';
+$lang['contact-title']='简易联系框';
+$lang['contact-user']='Name';
+$lang['contact-unit']='Company';
+$lang['contact-email']='E-mail';
+$lang['contact-phone']='Telephone';
+$lang['contact-message']='Message';
+$lang['main-product']='主要产品';
+$lang['aboutus']='关于菲尔特';
+$lang['site-links']='相关网站';
+$lang['site-biz']='网站事务';
+$lang['s1'] ='应用领域';
+$lang['s11'] ='纺织';
+$lang['s111'] ='纯金属纤维线';
+$lang['s112'] ='金属纤维混纺纱';
+$lang['s12'] ='汽车';
+$lang['s121'] ='柴油机尾气过滤';
+$lang['s122'] ='汽车座椅加热';
+$lang['s123'] ='汽车安全气囊';
+$lang['s13'] ='设备';
+$lang['s131'] ='燃烧器';
+$lang['s132'] ='喷墨打印机';
+$lang['s14'] ='化工';
+$lang['s141'] ='煤化工';
+$lang['s142'] ='稠化器';
+$lang['s15'] ='化纤';
+$lang['s16'] ='粘胶';
+$lang['s17'] ='导电塑料';
+$lang['s18'] ='太阳能';
+$lang['s19'] ='电子';
+$lang['s1110'] ='玻璃';
+$lang['s1111'] ='医药';
+$lang['s1112'] ='冶金';
+$lang['s2'] ='产品信息';
+$lang['s21'] ='金属纤维';
+$lang['s211'] ='Stainless steel fiber';
+$lang['s212'] ='Fecralloy fiber';
+$lang['s213'] ='镍纤维';
+$lang['s214'] ='哈氏合金纤维';
+$lang['s215'] ='锆纤维';
+$lang['s216'] ='压花纤维';
+$lang['s22'] ='纤维毡';
+$lang['s221'] ='Stainless steel fiber felt';
+$lang['s222'] ='Fecralloy fiber felt';
+$lang['s223'] ='非烧结毡';
+$lang['s224'] ='针刺毡';
+$lang['s225'] ='水刺毡';
+$lang['s23'] ='多层金属烧结网';
+$lang['s231'] ='五层金属烧结网';
+$lang['s24'] ='金属纤维制品';
+$lang['s241'] ='波纹状熔体滤芯';
+$lang['s242'] ='筒状滤芯';
+$lang['s243'] ='过滤碟片';
+$lang['s244'] ='高温气体除尘滤袋';
+$lang['s245'] ='太阳能回热器网芯';
+$lang['s246'] ='多层网片式过滤器';
+$lang['s247'] ='燃烧用编织网';
+$lang['s3'] ='技术研发';
+$lang['s31'] ='研发平台';
+$lang['s311'] ='State Key Laboratory of Metal Porous Materials';
+$lang['s312'] ='国家实验室认可的理化检测中心';
+$lang['s313'] ='Municipal Enterprise Technology Center';
+$lang['s32'] ='拥有技术';
+$lang['s321'] ='金属纤维技术';
+$lang['s322'] ='金属纤维毡技术';
+$lang['s323'] ='金属烧结网技术';
+$lang['s324'] ='各种滤器的设计及加工技术';
+$lang['s33'] ='研发项目和课题';
+$lang['s331'] ='金属纤维研发';
+$lang['s332'] ='金属纤维毡研发';
+$lang['s333'] ='金属烧结网研发';
+$lang['s4'] ='Quality Control';
+$lang['s41'] ='Quality Policy';
+$lang['s42'] ='Quality Management System';
+$lang['s43'] ='Strict analysis of the test system';
+$lang['s44'] ='Strict process control';
+$lang['s5'] ='公司概况';
+$lang['s51'] ='Brief Introduction';
+$lang['s52'] ='发展历程';
+$lang['s53'] ='企业文化';
+$lang['s54'] ='企业荣誉';
+$lang['s55'] ='领导关怀';
+$lang['s56'] ='社会责任';
+$lang['s561'] ='节能与环保';
+$lang['s562'] ='健康与安全';
+$lang['s57'] ='企业宣传册';
+$lang['s6'] ='Contact';
+$lang['s61'] ='关于我们';
+$lang['s62'] ='网站地图';
+$lang['s63'] ='常见问题';
+
+$lang['home-a-name']='R & D Strength';
+$lang['home-a-html']='<div class="yf_dx"></div>
+        <div class="yf_01">
+            <div class="yf_img"><img src="/resources/images/common/yfimg01.jpg" alt="R & D Strength" /></div>
+            <h1><a href="/welcome/catalog/311">多孔材料国家实验室</a></h1>
+            <h2>The laboratory mainly focuses on the research of metal fiber technology, metal fiber porous materials technology as well as theoretical research on material property and application, providing a solid foundation to enhance the company\'s R & D capabilities.</h2>
+        </div>
+        <div class="yf_01">
+            <div class="yf_img"><img src="/resources/images/common/yfimg02.jpg" alt="R & D Strength" /></div>
+            <h1><a href="/welcome/catalog/312">Municipal Enterprise Technology Center</a></h1>
+            <h2>The technology center accomplishes unique core proprietary technologies in product development, management innovation, technological innovation, industrial automation, energy-saving and emission reduction, which has played an active role in the rapid development of enterprise itself as well as the industry.</h2>
+        </div>';
+
+
+
+$lang['home-b-name']='Brief Introduction';
+$lang['home-b-html']=' <div class="gs_dx"></div>
+        <div class="gs_info">
+            <h1>	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Xi’an Filter Metal Materials Co., Ltd. is the global leading R&D and manufacturing company for metal fiber and fiber products. The main products include metal fiber, fiber felt, multi-layer sintered wire mesh, filter elements and other porous metal materials, widely applied in fields of chemical fiber, petrochemical, textile, environmental protection, automobile, electrical appliance etc.</h1>
+        </div>
+        <div class="gs_info02">
+            <ul>
+                <li>Founded in 1993</li>
+                <li>Largest metal fiber and fiber products supplier in China</li>
+                <li>Annual capacity of 300 tons metal fiber</li>
+                <li>Annual capacity of 150,000 m2 sintered filtration materials</li>
+                <li>Executive member of State Key Laboratory of Metal Porous Materials</li>
+                <li>Undertaking 15 state and provincial key R&D　projects</li>
+                <li>Authorized 9 patents</li>
+                <li>Subsidiary company of Western Metal materials Co.,Ltd. （Public listed company in Shenzhen Stock Exchange of China, stock code 002149）</li>
+            </ul>
+        </div>';
+
+
+$lang['home-c-name']='Contact';
+$lang['home-c-html']='<div class="call_dx"></div>
+        <div class="call_info">
+            <h1>Address: Xi\'an Economic and Technological Development Zone, China Jingwei Industrial Park West Golden Road 15</h1>
+            <h1>Postal Code: 710201</h1>
+            <h1>Tel 029 -86968425 029-86968420</h1>
+            <h1>Fax 029 -86968425 029-86968420</h1>
+            <h1>E-MAIL: xianfilter@yahoo.com.cn (Domestic) xianfilter@gmail.com (International) </h1>
+            <h1>Website: www.c-frt.com</h1>
+        </div>';
+
+$lang['home-d-name']='Featured Products';
+$lang['home-d-html']='<div class="cp_dx"></div>
+        <div class="cpimg">
+            <ul>
+                <li><img src="/resources/images/common/cpimg01.jpg" alt="Featured Products" /></li>
+                <li><img src="/resources/images/common/cpimg02.jpg" alt="Featured Products" /></li>
+                <li><img src="/resources/images/common/cpimg03.jpg" alt="Featured Products" /></li>
+            </ul>
+        </div>
+        <div class="cpinfo">
+            <ul>
+
+                <li><a target="_blank" href="/welcome/catalog/221">Stainless steel fiber felt</a></li>
+                <li><a target="_blank" href="/welcome/catalog/222">Fecralloy fiber felt</a></li>
+                <li><a target="_blank" href="/welcome/catalog/223">非烧结毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/224">针刺毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/225">水刺毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/211">Stainless steel fiber</a></li>
+                <li><a target="_blank" href="/welcome/catalog/212">Fecralloy fiber</a></li>
+
+
+            </ul>
+        </div>';
+
+
+$lang['home-e-name']='Featured Application';
+$lang['home-e-html']=' <div class="yy_dx"></div>
+        <div class="yy_info">
+            <h1>Automobile exhaust purification carrier</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg01.jpg" alt="Featured Application" /></div>
+                <h2>Fecralloy fiber felt is an excellent material for the production of automobile exhaust purification with particulate trap (DPF), it has a series of advantages such as high geometric surface area to improve the amount of catalyst adsorption, high porosity and through-hole rate to improve the full amount of combustion gases, high thermal conductivity to improve carrier rapid heating performance, high temperature and oxidation resistance, high strength of the substrate, good heat vibration performance etc.</h2>
+            </div>
+        </div>
+        <div class="yy_info">
+            <h1>Burner</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg02.jpg" alt="Featured Application" /></div>
+                <h2>Burner head is placed a layer of metal fiber braid mesh, the gas burned in the combustion chamber through the metal fiber braid mesh. Metal fibers can lead to uniform airflow, the gas - air mixture stream flowing from the small gap of tiny metal fibers, as well as good heat transfer characteristics, so that the combustion heat can rapidly disseminate through radiation and convection heat transfer, so as to effectively control the temperature distribution of the combustion chamber, avoiding partial high temperatures in the combustion chamber.</h2>
+            </div>
+        </div>
+        <div class="yy_info">
+            <h1>Dust filtration in high temperature gas</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg03.jpg" alt="Featured Application" /></div>
+                <h2>Fecralloy fiber felt has high filtration efficiency, high porosity, low resistance, sufficient strength and toughness, good for molding process, high temperature resistance (working temperatures up to 1000 ° C), outstanding anticorrosive performance; better soot filtering effectiveness and efficiency, better regeneration capacity, long life, significant energy-saving effect.</h2>
+            </div>
+        </div>';
+
+
+
+$lang['home-f-name']='Quality Control';
+$lang['home-f-html']='<div class="zl_dx"></div>
+        <div class="zl_info">
+            <ul>
+                <li><a href="/welcome/catalog/41">Quality Policy</a></li>
+                <li><a href="/welcome/catalog/42">Quality Management System</a></li>
+                <li><a href="/welcome/catalog/43">Strict analysis of the test system</a></li>
+                <li><a href="/welcome/catalog/44">Strict process control</a></li>
+            </ul>
+        </div>';
+
+
+
+$lang['home-g-name']='Related Links';
+$lang['home-g-html']=' <div class="zl_dx"></div>
+        <div class="zl_info">
+            <ul>
+                <li><a href="http://www.c-wmm.com"> www.c-wmm.com</a></li>
+                <li><a href="http://www.c-nin.com">www.c-nin.com</a></li>
+                <li><a href="http://www.c-wst.com">www.c-wst.com</a></li>
+                <li><a href="http://www.c-tlc.com">www.c-tlc.com</a></li>
+
+            </ul>
+        </div>';
+
+
+$lang['contact-details']='  <ul>
+            <li>Address: Xi\'an Economic and Technological Development Zone, China Jingwei Industrial Park West Golden Road 15</li>
+            <li>Postal Code: 710201</li>
+            <li>Tel 029 -86968425 029-86968420</li>
+            <li>Fax 029 -86968425 029-86968420</li>
+            <li>E-MAIL: xianfilter@yahoo.com.cn (Domestic) </li>
+            <li> xianfilter@gmail.com (International)</li>
+            <li>Website: www.c-frt.com</li>
+
+
+        </ul>';
+
+
+
--- application/language/ja/ja_lang.php
+++ application/language/ja/ja_lang.php
@@ -6,4 +6,231 @@
  * Time: 上午11:01
  * To change this template use File | Settings | File Templates.
  */
-$lang['site_name'] ="Xi'an Filter";
+$lang['site_name'] ="西安菲尔特";
+$lang['copyright'] = '版权所有 西安菲尔特有限公司 1993-2013。 保留一切权利。陕ICP备06013362号';
+$lang['contact-title']='简易联系框';
+$lang['contact-user']='名称';
+$lang['contact-unit']='単位';
+$lang['contact-email']='Eメール';
+$lang['contact-phone']='電話番号';
+$lang['contact-message']='情報';
+$lang['main-product']='主要产品';
+$lang['aboutus']='关于菲尔特';
+$lang['site-links']='相关网站';
+$lang['site-biz']='网站事务';
+$lang['s1'] ='应用领域';
+$lang['s11'] ='纺织';
+$lang['s111'] ='纯金属纤维线';
+$lang['s112'] ='金属纤维混纺纱';
+$lang['s12'] ='汽车';
+$lang['s121'] ='柴油机尾气过滤';
+$lang['s122'] ='汽车座椅加热';
+$lang['s123'] ='汽车安全气囊';
+$lang['s13'] ='设备';
+$lang['s131'] ='燃烧器';
+$lang['s132'] ='喷墨打印机';
+$lang['s14'] ='化工';
+$lang['s141'] ='煤化工';
+$lang['s142'] ='稠化器';
+$lang['s15'] ='化纤';
+$lang['s16'] ='粘胶';
+$lang['s17'] ='导电塑料';
+$lang['s18'] ='太阳能';
+$lang['s19'] ='电子';
+$lang['s1110'] ='玻璃';
+$lang['s1111'] ='医药';
+$lang['s1112'] ='冶金';
+$lang['s2'] ='产品信息';
+$lang['s21'] ='金属纤维';
+$lang['s211'] ='ステンレス鋼繊維';
+$lang['s212'] ='アイアンブランドアルミニウム繊維';
+$lang['s213'] ='镍纤维';
+$lang['s214'] ='哈氏合金纤维';
+$lang['s215'] ='锆纤维';
+$lang['s216'] ='压花纤维';
+$lang['s22'] ='纤维毡';
+$lang['s221'] ='ステンレス鋼繊維のフェルト';
+$lang['s222'] ='アイアンブランドアルミニウム繊維フェルト';
+$lang['s223'] ='非烧结毡';
+$lang['s224'] ='针刺毡';
+$lang['s225'] ='水刺毡';
+$lang['s23'] ='多层金属烧结网';
+$lang['s231'] ='五层金属烧结网';
+$lang['s24'] ='金属纤维制品';
+$lang['s241'] ='波纹状熔体滤芯';
+$lang['s242'] ='筒状滤芯';
+$lang['s243'] ='过滤碟片';
+$lang['s244'] ='高温气体除尘滤袋';
+$lang['s245'] ='太阳能回热器网芯';
+$lang['s246'] ='多层网片式过滤器';
+$lang['s247'] ='燃烧用编织网';
+$lang['s3'] ='技术研发';
+$lang['s31'] ='研发平台';
+$lang['s311'] ='多孔質材料の国立研究所';
+$lang['s312'] ='国家实验室认可的理化检测中心';
+$lang['s313'] ='市営企業技術センター';
+$lang['s32'] ='拥有技术';
+$lang['s321'] ='金属纤维技术';
+$lang['s322'] ='金属纤维毡技术';
+$lang['s323'] ='金属烧结网技术';
+$lang['s324'] ='各种滤器的设计及加工技术';
+$lang['s33'] ='研发项目和课题';
+$lang['s331'] ='金属纤维研发';
+$lang['s332'] ='金属纤维毡研发';
+$lang['s333'] ='金属烧结网研发';
+$lang['s4'] ='品質管理';
+$lang['s41'] ='品質方針';
+$lang['s42'] ='品質管理システム';
+$lang['s43'] ='検出システムの厳密な分析';
+$lang['s44'] ='厳密なプロセス制御';
+$lang['s5'] ='公司概况';
+$lang['s51'] ='会社の概要';
+$lang['s52'] ='发展历程';
+$lang['s53'] ='企业文化';
+$lang['s54'] ='企业荣誉';
+$lang['s55'] ='领导关怀';
+$lang['s56'] ='社会责任';
+$lang['s561'] ='节能与环保';
+$lang['s562'] ='健康与安全';
+$lang['s57'] ='企业宣传册';
+$lang['s6'] ='連絡';
+$lang['s61'] ='关于我们';
+$lang['s62'] ='网站地图';
+$lang['s63'] ='常见问题';
+
+$lang['home-a-name']='R＆D';
+$lang['home-a-html']='<div class="yf_dx"></div>
+        <div class="yf_01">
+            <div class="yf_img"><img src="/resources/images/common/yfimg01.jpg" alt="R＆D" /></div>
+            <h1><a href="/welcome/catalog/311">多孔質材料の国立研究所</a></h1>
+            <h2>金属繊維の調製技術、金属繊維多孔質材料の準備理論材料特性と理論研究のアプリケーションを実行するための研究所は、同社のR＆D機能を強化するための強固な基盤を築きました。</h2>
+        </div>
+        <div class="yf_01">
+            <div class="yf_img"><img src="/resources/images/common/yfimg02.jpg" alt="R＆D" /></div>
+            <h1><a href="/welcome/catalog/312">市営企業技術センター</a></h1>
+            <h2>製品開発における技術センター、経営革新、技術革新、産業オートメーション、省エネ•排出削減、独自のコア独自の技術の形成は、企業自体に積極的な役割だけでなく、産業の急速な発展を果たしてきました。</h2>
+        </div>';
+
+
+
+$lang['home-b-name']='会社の概要';
+$lang['home-b-html']=' <div class="gs_dx"></div>
+        <div class="gs_info">
+            <h1>	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;フュルト金属フィルター材料有限公司の西安は、金属繊維およびその製品のR＆Dと製造企業におけるグローバルリーダーです。主な製品は金属繊維であり、金属繊維のフェルト、焼結金属、金属フィルターや他の多孔性の金属材料の焼結金網層は、広く化学繊維、石油化学、繊維、環境保護、自動車、電気、および他の分野に適用される。</h1>
+        </div>
+        <div class="gs_info02">
+            <ul>
+                <li>1993年に設立され</li>
+                <li>同国最大の金属繊維およびその製品の生産拠点。</li>
+                <li>金属繊維の300トンの年間生産量</li>
+                <li>15万平方メートル年は焼結フィルター</li>
+                <li>ナショナル多孔性材料研究室サポートユニット</li>
+                <li>クマ15国と地方の主要プロジェクト</li>
+                <li>9認可された特許</li>
+                <li>西部材の保持子会社（深セン証券取引所上場企業、証券コード002149）</li>
+            </ul>
+        </div>';
+
+
+$lang['home-c-name']='連絡';
+$lang['home-c-html']='<div class="call_dx"></div>
+        <div class="call_info">
+            <h1>住所：中国西安経済技術開発区、兆銘工業団地ウエストゴールデンロード15</h1>
+            <h1>郵便番号：710201</h1>
+            <h1>電話029 -86968425   029-86968420</h1>
+            <h1>ファックス029 -86968425029-86968420</h1>
+            <h1>E-MAIL：xianfilter@yahoo.com.cn（国内）xianfilter@gmail.com（国際）</h1>
+            <h1>ウェブサイト：www.c-frt.com</h1>
+        </div>';
+
+$lang['home-d-name']='特産品';
+$lang['home-d-html']='<div class="cp_dx"></div>
+        <div class="cpimg">
+            <ul>
+                <li><img src="/resources/images/common/cpimg01.jpg" alt="特産品" /></li>
+                <li><img src="/resources/images/common/cpimg02.jpg" alt="特産品" /></li>
+                <li><img src="/resources/images/common/cpimg03.jpg" alt="特産品" /></li>
+            </ul>
+        </div>
+        <div class="cpinfo">
+            <ul>
+
+                <li><a target="_blank" href="/welcome/catalog/221">ステンレス鋼繊維のフェルト</a></li>
+                <li><a target="_blank" href="/welcome/catalog/222">アイアンブランドアルミニウム繊維フェルト</a></li>
+                <li><a target="_blank" href="/welcome/catalog/223">非烧结毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/224">针刺毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/225">水刺毡</a></li>
+                <li><a target="_blank" href="/welcome/catalog/211">ステンレス鋼繊維</a></li>
+                <li><a target="_blank" href="/welcome/catalog/212">アイアンブランドアルミニウム繊維</a></li>
+
+
+            </ul>
+        </div>';
+
+
+$lang['home-e-name']='注目のアプリケーション';
+$lang['home-e-html']=' <div class="yy_dx"></div>
+        <div class="yy_info">
+            <h1>自動車排ガス浄化キャリア</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg01.jpg" alt="注目のアプリケーション" /></div>
+                <h2>アイアンブランド触媒吸着、高気孔率、およびスルーホールの度合燃焼ガスの全額、熱伝導率の高いキャリア急速加熱性能、耐高温酸化性、基材の高強度、熱振動性能を向上させるために高い幾何学的表面積を有するアルミニウム繊維マットかなりの利益、微粒子トラップ装置（DPF）を持つ自動車の排気ガス浄化の生産のための優れた材料。</h2>
+            </div>
+        </div>
+        <div class="yy_info">
+            <h1>バーナー</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg02.jpg" alt="注目のアプリケーション" /></div>
+                <h2>バーナーヘッドは、金属繊維編組、金属繊維編組を通って燃焼室で燃焼ガスの層が配置されている。効果的に燃焼室の温度分布を制御するために、放射と対流熱伝達急速な普及によってとても小さい金属繊維の小さなギャップだけでなく、良好な熱伝達特性、その燃焼熱から流れる空気の混合気の流れ、 - 金属繊維は均一な気流、ガスであってもよい、燃焼室内の局所高温を避ける。</h2>
+            </div>
+        </div>
+        <div class="yy_info">
+            <h1>高温ガス粉塵</h1>
+            <div class="yy_info02">
+                <div class="yy_info_img"><img src="/resources/images/common/yyimg03.jpg" alt="注目のアプリケーション" /></div>
+                <h2>繊維マットの鉄のブランドアルミろ過効率、高気孔率、低抵抗は、成形プロセスを容易にするために、十分な強度と靭性を持っているが、防錆性能、（1000℃までの温度に使用）、良好な高温があり、煤が良い持っているフィルタリングの有効性と効率性、優れた再生能力、長寿命、省エネ効果が顕著である.</h2>
+            </div>
+        </div>';
+
+
+
+$lang['home-f-name']='品質管理';
+$lang['home-f-html']='<div class="zl_dx"></div>
+        <div class="zl_info">
+            <ul>
+                <li><a href="/welcome/catalog/41">品質方針</a></li>
+                <li><a href="/welcome/catalog/42">品質管理システム</a></li>
+                <li><a href="/welcome/catalog/43">検出システムの厳密な分析</a></li>
+                <li><a href="/welcome/catalog/44">厳密なプロセス制御</a></li>
+            </ul>
+        </div>';
+
+
+
+$lang['home-g-name']='関連リンク';
+$lang['home-g-html']=' <div class="zl_dx"></div>
+        <div class="zl_info">
+            <ul>
+                <li><a href="http://www.c-wmm.com"> www.c-wmm.com</a></li>
+                <li><a href="http://www.c-nin.com">www.c-nin.com</a></li>
+                <li><a href="http://www.c-wst.com">www.c-wst.com</a></li>
+                <li><a href="http://www.c-tlc.com">www.c-tlc.com</a></li>
+
+            </ul>
+        </div>';
+
+
+$lang['contact-details']='  <ul>
+            <li>住所：中国西安経済技術開発区、兆銘工業団地ウエストゴールデンロード15</li>
+            <li>郵便番号：710201</li>
+            <li>電話029 -86968425   029-86968420</li>
+            <li>ファックス029 -86968425029-86968420</li>
+            <li>E-MAIL：xianfilter@yahoo.com.cn（国内）</li>
+            <li>xianfilter@gmail.com（国際）</li>
+			<li>ウェブサイト：www.c-frt.com</li>
+
+
+        </ul>';
+
+
+
